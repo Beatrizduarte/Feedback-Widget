cfcf5ad46382eef4da3231c87b36d06405d1ce96
beatrizDuarte <beatriznduarte1@gmail.com>
2022-07-04 20:23:01 -0300

    Feature: Create screenshot button, input form and send button

--- mobile/src/components/Form/index.tsx
+++ mobile/src/components/Form/index.tsx
@@ -8,10 +8,20 @@ import {
 } from 'react-native';
 import { ArrowLeft } from 'phosphor-react-native';
 
+import { FeedbackType } from '../../components/Widget'
+import { Button } from '../Button';
+import { ScreenshotButton } from '../ScreenshotButton';
+
 import { styles } from './styles';
 import { theme } from '../../theme';
+import { feedbackTypes } from '../../utils/feedbackTypes';
 
-export function Form() {
+interface Props {
+  feedbackType: FeedbackType;
+}
+
+export function Form({ feedbackType }: Props) {
+  const feedbackTypeInfo = feedbackTypes[feedbackType];
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -24,10 +34,33 @@ export function Form() {
         </TouchableOpacity>
 
         <View style={styles.titleContainer}>
+          <Image 
+            source={feedbackTypeInfo.image}
+            style={styles.image}
+          />
           <Text style={styles.titleText}>
-            
+            {feedbackTypeInfo.title}
           </Text>
         </View>
+      </View>
+    
+      <TextInput 
+        multiline
+        style={styles.input}
+        placeholder="Algo não está funcionando bem? Queremos corrigir. Conte com detalhes o que está acontecendo..."
+        placeholderTextColor={theme.colors.text_secondary}
+      />
+
+      <View style={styles.footer}>
+        <ScreenshotButton 
+          onTakeShot={() => {}}
+          onRemoveShot={() => {}}
+          screenshot=""
+        />
+
+        <Button 
+          isLoading={false}
+        />
       </View>
     </View>
   );
